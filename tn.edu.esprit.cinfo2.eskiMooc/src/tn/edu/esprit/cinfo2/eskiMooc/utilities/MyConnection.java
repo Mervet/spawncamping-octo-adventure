b3ab2102ce8a965c54e5cbab4a330b9b3989b8be
Merfet <Merfet@Merfet-PC>
2014-03-17 20:32:35 +0100

    Signed-off-by: Merfet <Merfet@Merfet-PC>

--- tn.edu.esprit.cinfo2.eskiMooc/src/tn/edu/esprit/cinfo2/eskiMooc/utilities/MyConnection.java
+++ tn.edu.esprit.cinfo2.eskiMooc/src/tn/edu/esprit/cinfo2/eskiMooc/utilities/MyConnection.java
@@ -1,5 +1,30 @@
 package tn.edu.esprit.cinfo2.eskiMooc.utilities;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MyConnection {
+
+	private Connection connection;
+
+	public Connection hetConnection() {
+		String url = "";
+		String user = "";
+		String password = "";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(url, user, password);
+			System.out.println("connection established");
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return connection;
+	}
 
 }
