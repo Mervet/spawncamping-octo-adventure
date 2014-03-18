11efe0e0c6c4bb44b1133a1e97c9b54bf27e754e
Merfet <Merfet@172.16.1.236>
2014-03-18 20:14:32 +0100

    schema buckup
    Signed-off-by: Merfet <Merfet@172.16.1.236>

---
+++ tn.edu.esprit.cinfo2.eskiMooc/base/eskimocdb 20140318 2012.sql
@@ -0,0 +1,73 @@
+-- MySQL Administrator dump 1.4
+--
+-- ------------------------------------------------------
+-- Server version	5.5.24-log
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+--
+-- Create schema eskimoocdb
+--
+
+CREATE DATABASE IF NOT EXISTS eskimoocdb;
+USE eskimoocdb;
+
+--
+-- Definition of table `t_skill`
+--
+
+DROP TABLE IF EXISTS `t_skill`;
+CREATE TABLE `t_skill` (
+  `idT_SKILL` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `label` varchar(45) NOT NULL,
+  PRIMARY KEY (`idT_SKILL`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_skill`
+--
+
+/*!40000 ALTER TABLE `t_skill` DISABLE KEYS */;
+INSERT INTO `t_skill` (`idT_SKILL`,`label`) VALUES 
+ (1,'php');
+/*!40000 ALTER TABLE `t_skill` ENABLE KEYS */;
+
+
+--
+-- Definition of table `t_user`
+--
+
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
+  `idT_USER` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `firstName` varchar(45) NOT NULL,
+  PRIMARY KEY (`idT_USER`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_user`
+--
+
+/*!40000 ALTER TABLE `t_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_user` ENABLE KEYS */;
+
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
